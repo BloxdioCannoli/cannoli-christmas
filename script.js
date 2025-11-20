73eb65c2cbe3206be949e629b9368dd12390558a
BloxdioCannoli <coding.cannoli@gmail.com>
2025-11-20 18:59:12 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+const width = canvas.width = window.innerWidth
+const height = canvas.height = window.innerHeight
+const centerX = width / 2
+const centerY = height / 2
+
+const rectHeight = 100;
+const speed=0.05;
+const rotation=25;
+
+
+let offset = -centerY;
+
+
+colors = ["red", "white"]
+
+ctx.rotate((Math.PI / 180) * rotation)
+ctx.translate(centerX, centerY)
+
+setInterval(function() {
+  ctx.clearRect(0-centerX, 0-centerY, width+centerX, height-centerY)
+
+  for (let r = -1; r <= (height + -offset) / rectHeight; r++) {
+    ctx.fillStyle = colors[(r + 1) % colors.length];
+    ctx.fillRect(0-centerX, rectHeight * r - -offset-centerY, width+centerX, rectHeight+centerY);
+  }
+
+  offset-=speed;
+
+}, 1)

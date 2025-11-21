d9e2c702ab6986d3cf27d9dbf129389f1e0eb918
BloxdioCannoli <coding.cannoli@gmail.com>
2025-11-20 21:50:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-const canvas = document.getElementById("canvas")
-const ctx = canvas.getContext("2d")
-const width = canvas.width = window.innerWidth
-const height = canvas.height = window.innerHeight
-const centerX = width / 2
-const centerY = height / 2
+window.onload = () => {
+  const canvas = document.getElementById("canvas")
+  const ctx = canvas.getContext("2d")
+  const width = canvas.width = window.innerWidth
+  const height = canvas.height = window.innerHeight
+  const centerX = width / 2
+  const centerY = height / 2
 
-const rectHeight = 100;
-const speed=0.05;
-const rotation=25;
-
-
-let offset = -centerY;
+  const rectHeight = 100;
+  const speed = 0.05;
+  const rotation = 25;
 
 
-colors = ["red", "white"]
+  let offset = -centerY;
 
-ctx.rotate((Math.PI / 180) * rotation)
-ctx.translate(centerX, centerY)
+  const rootStyles = getComputedStyle(document.documentElement);
 
-setInterval(function() {
-  ctx.clearRect(0-centerX, 0-centerY, width+centerX, height-centerY)
+  const primary = rootStyles.getPropertyValue("--primary");
+  const secondary = rootStyles.getPropertyValue("--secondary");
 
-  for (let r = -1; r <= (height + -offset) / rectHeight; r++) {
-    ctx.fillStyle = colors[(r + 1) % colors.length];
-    ctx.fillRect(0-centerX, rectHeight * r - -offset-centerY, width+centerX, rectHeight+centerY);
+
+  colors = [primary, secondary]
+
+  ctx.rotate((Math.PI / 180) * rotation)
+  ctx.translate(centerX, centerY)
+
+  setInterval(function() {
+    ctx.clearRect(0 - centerX, 0 - centerY, width + centerX, height - centerY)
+
+    for (let r = -1; r <= (height + -offset) / rectHeight; r++) {
+      ctx.fillStyle = colors[(r + 1) % colors.length];
+      ctx.fillRect(0 - centerX, rectHeight * r - -offset - centerY, width + centerX, rectHeight + centerY);
+    }
+
+    offset -= speed;
+
+  }, 1)
+
+  const calendar = document.getElementById("calendar")
+  for (let cb = 0; cb < 24; cb++) {
+    calendar.innerHTML += `
+  <div class="calander-box">
+    Day ${cb+1}
+  </div>
+  `
   }
-
-  offset-=speed;
-
-}, 1)
+}

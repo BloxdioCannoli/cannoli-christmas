14ad4ca18257718ced87f6fb471a3fd10806e0c4
BloxdioCannoli <coding.cannoli@gmail.com>
2025-11-22 10:32:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -36,17 +36,58 @@ window.onload = () => {
 
   }, 1)
 
+  const currentDay = 1
+
+  const special = rootStyles.getPropertyValue("--special");
+
+  const specialDays=[4, 8, 12, 16, 20, 24]
+
+
   const calendar = document.getElementById("calendar")
   for (let cb = 0; cb < 24; cb++) {
-    calendar.innerHTML += `
-  <div class="calander-box">
-    Day ${cb+1}
+    let isActive = (currentDay > cb)
+    let isCurrent = (currentDay == cb+1)
+    let isSpecial = (specialDays.includes(cb+1))
+    
+    calendar.insertAdjacentHTML("beforeend", `
+  <div class="calendar-box">
+    <span class="day-display">Day ${cb + 1}</span>
+      <div class="cropped-box">
+        <div class="image-cover"></div>
+        <img src="Screenshot 2025-11-21 at 1.13.08 PM.png" height="100" width="90" draggable="false" />
+      </div>
   </div>
-  `
-  }
+  `)
+    document.getElementsByClassName("cropped-box")[cb].style.filter = (currentDay > cb + 1) ? `blur(0)` : `blur(${(cb + 1) - currentDay}px)`
 
-  const spec = window.location.href.split("?")[1]?.split(",")
-  if (spec.includes("hideteaser")) {
-    document.getElementById("teaser").style.display="none"
+    if (!isActive) {
+      document.getElementsByClassName("calendar-box")[cb].style.cursor = "not-allowed"
+      document.getElementsByClassName("cropped-box")[cb].style.filter = `blur(${((cb + 1) - currentDay-3 <= 9) ? (cb + 1) - currentDay +3 : 9}px)`
+      document.getElementsByClassName("cropped-box")[cb].style.backgroundColor = `rgba(0,0,0,0.5)`
+      document.getElementsByClassName("image-cover")[cb].style.background = `rgba(0, 0, 0, 0.3)`
+      if (isSpecial) {
+        //document.getElementsByClassName("image-cover")[cb].style.background = `rgba(150, 150, 0, 0.3)`
+        document.getElementsByClassName("day-display")[cb].style.fontWeight="600"
+        document.getElementsByClassName("day-display")[cb].style.fontSize="15px"
+      }
+    } else {
+      document.getElementsByClassName("calendar-box")[cb].style.cursor = "pointer"
+      document.getElementsByClassName("cropped-box")[cb].style.filter = `blur(0)`
+      document.getElementsByClassName("cropped-box")[cb].style.backgroundColor = `rgba(255,255,255,0.5)`
+      document.getElementsByClassName("image-cover")[cb].style.background = `rgba(255, 255, 255, 0.1)`
+
+      document.getElementsByClassName("calendar-box")[cb].onclick=function() {window.open(`https://bloxdiocannoli.github.io/cannoli-christmas/codes/codes?${cb+1}`)}
+
+      if (isSpecial) {
+        //document.getElementsByClassName("image-cover")[cb].style.background = `rgba(255, 255, 0, 0.3)`
+        document.getElementsByClassName("day-display")[cb].style.fontWeight="600"
+        document.getElementsByClassName("day-display")[cb].style.fontSize="15px"
+      }
+
+      if (isCurrent) {
+        document.getElementsByClassName("day-display")[cb].style.textDecoration="underline"
+        
+      }
+    }
   }
 }
